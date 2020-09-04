7576df16b427fca36f2eda80a2b10994416a771f
DC <caruso.denny@gmail.com>
2020-09-04 13:31:57 +0200

    Initial Commit

---
+++ dailyCodingProblem162/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  dailyCodingProblem162
+//
+//  Created by Denny Caruso on 04/09/2020.
+//  Copyright © 2020 Denny Caruso. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
