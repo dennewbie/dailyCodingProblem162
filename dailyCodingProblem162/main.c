6c710ee0548add0387f4d166925a0a488eb86697
DC <caruso.denny@gmail.com>
2020-09-04 14:02:09 +0200

    Final Push that solves the entire problem

--- dailyCodingProblem162/main.c
+++ dailyCodingProblem162/main.c
@@ -6,10 +6,81 @@
 //  Copyright © 2020 Denny Caruso. All rights reserved.
 //
 
+/*
+    Daily Coding Problem 162 - Level: Medium
+    This problem was asked by Square.
+
+    Given a list of words, return the shortest unique prefix of each word. For example, given the list:
+
+    - dog
+    - cat
+    - apple
+    - apricot
+    - fish
+    - duck
+ 
+    Return the list:
+
+    - do
+    - c
+    - app
+    - apr
+    - f
+    - du
+ 
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char **getShortestUniquePrefixArray(char **, int);
+void generateError(char *);
+void printArray(char **, int, char *);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char *wordsArray[] = { "Dog" , "Cat", "Apple", "Apricot", "Fish", "Duck" };
+    int size = (sizeof(wordsArray) / sizeof(wordsArray[0]));
+    
+    printArray(wordsArray, size, "\nWord List:\n");
+    char **finalArray = getShortestUniquePrefixArray(wordsArray, size);
+    printArray(finalArray, size, "\nFinal Word List:\n");
+    
     return 0;
+}
+
+char **getShortestUniquePrefixArray(char **wordList, int size) {
+    char **finalList = (char **) malloc(sizeof(char *) * size);
+    if(!finalList) generateError("final List");
+    unsigned short int nChar = 1;
+    
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i != j) {
+                if (strncmp(wordList[i], wordList[j], nChar) == 0) {
+                    nChar += 1;
+                    j--;
+                }
+            }
+        }
+        
+        finalList[i] = (char *) malloc(nChar + 1);
+        if (!finalList[i]) generateError("finalList[i]");
+        
+        strncpy(finalList[i], wordList[i], nChar);
+        finalList[i][nChar + 1] = '\0';
+        nChar = 1;
+    }
+    
+    return finalList;
+}
+
+void generateError(char *errorMessage) {
+    printf("\nError allocating memory for %s...\n", errorMessage);
+    exit(EXIT_FAILURE);
+}
+
+void printArray(char **finalList, int size, char *message) {
+    printf("\n%s\n", message);
+    for (int i = 0; i < size; i++) printf("-> %s\n", finalList[i]);
 }
